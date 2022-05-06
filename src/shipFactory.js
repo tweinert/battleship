194551921a60f1f8f1c693c7587b0969217a5ead
Tait Weinert <tait.weinert@outlook.com>
2022-05-06 18:14:24 +0930

    ship tests

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,6 +1,19 @@
 class Ship {
-    constructor(size) {
+    constructor(size, hits = []) {
         this.size = size;
+        this.hits = hits;
+    }
+
+    hit(position) {
+        this.hits.push(position);
+    }
+
+    isSunk() {
+        if (this.hits.length == this.size) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -8,3 +8,22 @@ beforeEach(() => {
 it("Ship has length", () => {
     expect(testShip.size).toBe(4);
 });
+
+it("Ship can be hit", () => {
+    testShip.hit(2);
+    expect(testShip.hits).toContain(2);
+});
+
+it("Ship can be sunk", () => {
+    testShip.hit(1);
+    testShip.hit(2);
+    testShip.hit(3);
+    testShip.hit(4);
+    expect(testShip.isSunk()).toBe(true);
+});
+
+it("Doesn't sink with half hits", () => {
+    testShip.hit(1);
+    testShip.hit(2);
+    expect(testShip.isSunk()).toBe(false);
+})
