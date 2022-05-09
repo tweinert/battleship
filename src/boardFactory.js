b4581cfc27169cd04f5b5ca4ed65156cf68af3e5
Tait Weinert <tait.weinert@outlook.com>
2022-05-09 19:17:21 +0930

    board hit detection

--- src/boardFactory.js
+++ src/boardFactory.js
@@ -3,19 +3,35 @@ import Ship from './shipFactory';
 const SIZE = 10;
 
 class Board {    
-    constructor(grid = []) {
+    constructor(grid = [], hits = []) {
         this.grid = grid;
+        this.hits = hits;
         this.initialize();
     }
 
     initialize() {
-        // create grid
+        // create grid and hits
         this.grid = Array.from(Array(SIZE), () => new Array(SIZE));
+        this.hits = Array.from(Array(SIZE), () => new Array(SIZE));
     }
 
     placeShip(ship, row, col) {
         this.grid[row][col] = ship;
         return typeof(this.grid[row][col]);
+    }
+
+    receiveAttack(row, col) {
+        // check if ship is at coords
+        if (typeof this.grid[row][col] === "object" && this.grid[row][col] !== null) {
+            this.grid[row][col].hit(1);
+            // TODO need way to determine what ship part to hit
+            return true;
+        } else {
+            this.hits[row][col] = true;
+            return false;
+        }
+        // if true, send hit to ship
+        // if false, record coords of miss
     }
 }
 
--- src/boardFactory.test.js
+++ src/boardFactory.test.js
@@ -11,3 +11,9 @@ beforeEach(() => {
 it("Place ship at coords", () => {
     expect(board.placeShip(ship, 4, 4)).toBe("object");
 });
+
+it("Ship is detected at coords", () => {
+    board.placeShip(ship, 5, 5);
+    expect(board.receiveAttack(5, 5)).toBeTruthy();
+    expect(board.receiveAttack(8, 8)).toBeFalsy();
+});
