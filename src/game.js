b691bce86a6b362dc35645d66f35c95bcc76d65a
Tait Weinert <tait.weinert@outlook.com>
2022-05-19 19:05:34 +0930

    add ships

--- src/game.js
+++ src/game.js
@@ -7,11 +7,29 @@ let computerBoard;
 let player1;
 let player2;
 
+let shipSmallBoard1;
+let shipMedBoard1;
+let shipLargeBoard1;
+
+let shipSmallBoard2;
+let shipMedBoard2;
+let shipLargeBoard2;
+
 function startGame() {
     playerBoard = new Board();
     computerBoard = new Board();
     player1 = new Player(true);
     player2 = new Player(false);
+
+    shipSmallBoard1 = new Ship(2);
+    shipMedBoard1 = new Ship(3);
+    shipLargeBoard1 = new Ship(4);
+
+    shipSmallBoard2 = new Ship(2);
+    shipMedBoard2 = new Ship(3);
+    shipLargeBoard2 = new Ship(4);
+
+    placeShips();
 
     refreshGameBoard(playerBoard, true);
     refreshGameBoard(computerBoard, false);
@@ -27,6 +45,8 @@ function refreshGameBoard(board, isPlayer) {
     } else {
         boardDiv = document.querySelector("#computer-board");
     }
+
+    boardDiv.innerHTML = "";
 
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
@@ -53,6 +73,18 @@ function playerSendAttack(event) {
     computerBoard.receiveAttack(row, col);
 
     event.currentTarget.textContent = "X";
+
+    refreshGameBoard(computerBoard);
+}
+
+function placeShips() {
+    playerBoard.placeShip(shipSmallBoard1, 0, 0);
+    playerBoard.placeShip(shipMedBoard1, 0, 2);
+    playerBoard.placeShip(shipLargeBoard1, 0, 5);
+
+    computerBoard.placeShip(shipSmallBoard2, 0, 0);
+    computerBoard.placeShip(shipMedBoard2, 0, 2);
+    computerBoard.placeShip(shipLargeBoard2, 0, 5);
 }
 
 export default startGame;
